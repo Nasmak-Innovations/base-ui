967c1a6da78b5a245c82c0c70573eee37a834067
Nasmak-Innovations <63855708+Nasmak-Innovations@users.noreply.github.com>
2020-09-09 12:28:42 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ ReactDOM.render(
         <Provider store={store} >
             <App />
         </Provider>
-    </Router>, document.getElementById("app")
+    </Router>, document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
